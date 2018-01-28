edc40239397ef47cb3ddaff04c4f5be759295a00
Michel Keselbrener <michel.keselbrener@ezworld.co.il>
2018-01-28 16:02:41 +0200

    ello Java: 28/01/2018 15:57 Create a local development branch

--- java-hello-world/src/JavaHelloWorld.java
+++ java-hello-world/src/JavaHelloWorld.java
@@ -4,8 +4,12 @@ public class JavaHelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		  System.out.println("Hello Java: 28/01/2018 14:55");  
-		
-
+		  System.out.println("Hello Java: 28/01/2018 14:59");
+		  
+		  System.out.println("Hello Java: 28/01/2018 15:34 Create a local development branch");
+		  System.out.println("Hello Java: 28/01/2018 15:57 Create a local development branch");
+		  
+	
 	}
 
 }

b9591ffdaf32e23da0d100689ddb816a82528c0c
Michel Keselbrener <michel.keselbrener@ezworld.co.il>
2018-01-28 14:55:30 +0200

    Hello Java: 28/01/2018 14:55

---
+++ java-hello-world/src/JavaHelloWorld.java
@@ -0,0 +1,11 @@
+
+public class JavaHelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  System.out.println("Hello Java: 28/01/2018 14:55");  
+		
+
+	}
+
+}
